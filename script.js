8e19e870e2476ca20c6284dadccc5b487576dcb0
smitzkar <karl.e.schmitz@hotmail.com>
2022-06-22 08:17:21 +0200

    Add grid functionality via script.js inside loop

--- script.js
+++ script.js
@@ -8,14 +8,20 @@ const canvas = document.querySelector("div#canvas");
 
 function generateCanvas(size)
 {
+  // Set set the canvas columns and rows 
+  canvas.style.gridTemplateColumns = "50px " * size;
+  canvas.style.gridTemplateRows = "50px " * size;
+
   for (let j = 0; j < size; j++)
   {
     for (let i = 0; i < size; i++)
     {
       // Create a bunch of pixel divs
       pixel = document.createElement("div");
-      pixel.classList.add('pixel');
-      pixel.textContent = "hi"
+      pixel.classList.add("pixel");
+      pixel.textContent = "hi";
+      pixel.style.background = "green";
+      pixel.style.gridColumn = i + 1;
       canvas.appendChild(pixel);
       console.log(i);
     }
